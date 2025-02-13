e316bec4a1d634d63ecfedb52ba25197a2229f99
FA24-BCS-089 <fa24-bcs-089@cuilahore.edu.pk>
2025-02-13 16:57:15 +0500

    Add files via upload

---
+++ Fa24bcsb.java
@@ -0,0 +1,10 @@
+public class Fa24bcsb{
+
+	public static void main(String args[]){
+		
+		System.out.println("First java task");
+		System.out.print(First java task");
+		System.out.printf(First java task");
+	}
+
+}
---
+++ InputData.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class InputData{
+
+	public static void main(String args[]){
+		
+		Scanner sc= new Scanner(System.in);
+
+		int i=0;
+				
+		System.out.println("Enter integer:");
+		i = sc.nextInt();
+		System.out.printf("Value entered: %d\n", i);
+	
+		Scanner sc1= new Scanner(System.in);
+
+		String str=" ";
+
+		System.out.println("Enter a string:");
+		str = sc1.nextLine();
+		System.out.printf("String entered: %s\n", str);
+		
+
+//sc.nextByte();
+//sc.nextShort();
+
+//sc.next(); //for a single word
+//sc.nextLine(); //for complete line
+
+	}
+}
