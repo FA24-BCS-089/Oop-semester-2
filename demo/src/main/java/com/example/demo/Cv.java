820778690e93e03563a29a40167d7990a7688b06
Namirah Manzoor <fa24-bcs-089@cuilahore.edu.pk>
2025-11-17 17:33:08 +0500

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Cv.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Cv {
+    public static void main(String args[]){
+
+        System.out.println("Namirah Manzoor");
+        System.out.println("FA24-BCS-B");
+        System.out.println("Semester 2");
+        System.out.println("Department of Computer Science");
+    }
+}
---
+++ demo/src/main/java/com/example/demo/Fa24bcsb.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Fa24bcsb {
+    public static void main(String args[]){
+
+        System.out.println("First java task");
+        System.out.print("First java task");
+                System.out.printf("First java task");
+    }
+}
